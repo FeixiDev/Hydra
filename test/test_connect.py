a800ceef42926a11562e182942556baefcbe4f31
zane <zane.chen@feixitek.com>
2020-08-10 17:41:32 +0800

    Zane:add test_connect,change test_storage and __init

---
+++ test/test_connect.py
@@ -0,0 +1,70 @@
+import connect
+import os
+
+
+SSHHOST = '10.203.1.200'
+SSHPORT = '22'
+SSHUSER = 'root'
+SSHPASSWORD = 'password'
+TIMEOUT = 3
+
+TELHOST = '10.203.1.231'
+TELPORT = 23
+TELUSER = 'root'
+TELPASSWORD = 'Feixi@123'
+
+
+class TestConnSSH:
+
+    def setup_class(self):
+        self.ssh = connect.ConnSSH(SSHHOST, SSHPORT, SSHUSER, SSHPASSWORD, TIMEOUT)
+
+    def test_connect(self):
+        assert self.ssh._connect() == None
+        assert self.ssh.SSHConnection != None
+
+    def test_execute_command(self):
+        stsdir = self.ssh.execute_command('pwd')
+        assert stsdir['sts'] == 1
+
+    def test_ssh_connect(self):
+        self.ssh.SSHConnection = None
+        self.ssh.ssh_connect()
+        assert self.ssh.SSHConnection != None
+
+    def test_download(self):
+        self.ssh.execute_command('echo \'this is test\' >> /pytest.txt')
+        self.ssh.download('/pytest.txt', '/tmp/pytest.txt')
+        file = os.popen('cat /tmp/pytest.txt')
+        assert file.read() == 'this is test\n'
+        self.ssh.execute_command('rm /pytest.txt')
+        os.remove('/tmp/pytest.txt')
+
+    def test_upload(self):
+        os.system('echo \'this is test\' >> /tmp/pytest.txt')
+        self.ssh.upload('/tmp/pytest.txt', '/pytest.txt')
+        stsdir = self.ssh.execute_command('cat /pytest.txt')
+        assert stsdir['rst'] == b'this is test\n'
+        os.remove('/tmp/pytest.txt')
+        self.ssh.execute_command('rm /pytest.txt')
+
+    def test_close(self):
+        assert self.ssh.close() == None
+
+
+class TestConnTelnet:
+
+    def setup_class(self):
+    	self.telnet = connect.ConnTelnet(TELHOST, TELPORT, TELUSER, TELPASSWORD, TIMEOUT)
+
+    def test_connect(self):
+    	assert self.telnet._connect() == None
+
+    def test_execute_command(self):
+    	assert self.telnet.execute_command('lun show') == None
+
+    def test_telnet_connect(self):
+    	assert self.telnet.telnet_connect() == None
+
+    def test_close(self):
+    	assert self.telnet.close() == None
--- test/test_storage.py
+++ test/test_storage.py
@@ -22,40 +22,39 @@ class TestDebugLog:
 
 
 class TestStorage:
-    
+
     def setup_class(self):
-    	storage.init_telnet()
-    	self.storage = storage.Storage()
-    	self.oprt_id = s.get_oprt_id()
+        storage.init_telnet()
+        self.storage = storage.Storage()
+        self.oprt_id = s.get_oprt_id()
 
     def test_ex_telnet_cmd(self):
-    	assert self.storage.ex_telnet_cmd('pytest', 'lun show', self.oprt_id)
+        assert self.storage.ex_telnet_cmd('pytest', 'lun show', self.oprt_id)
 
     def test_lun_create(self):
-    	assert self.storage.lun_create() == None
-    	self.storage.lun_destroy('pytest_99')
+        assert self.storage.lun_create() == None
+        self.storage.lun_destroy('pytest_99')
 
     def test_lun_map(self):
-    	self.storage.lun_create()
-    	assert self.storage.lun_map() == None
-    	self.storage.lun_unmap('pytest_99')
-    	self.storage.lun_destroy('pytest_99')
+        self.storage.lun_create()
+        assert self.storage.lun_map() == None
+        self.storage.lun_unmap('pytest_99')
+        self.storage.lun_destroy('pytest_99')
 
     def test_lun_unmap(self):
-    	self.storage.lun_create()
-    	self.storage.lun_map()
-    	assert self.storage.lun_unmap('pytest_99') == True
-    	self.storage.lun_destroy('pytest_99')
+        self.storage.lun_create()
+        self.storage.lun_map()
+        assert self.storage.lun_unmap('pytest_99') == True
+        self.storage.lun_destroy('pytest_99')
 
     def test_lun_destroy(self):
-    	self.storage.lun_create()
-    	assert self.storage.lun_destroy('pytest_99') == True
+        self.storage.lun_create()
+        assert self.storage.lun_destroy('pytest_99') == True
 
     def test_get_all_cfgd_lun(self):
-    	assert self.storage.get_all_cfgd_lun()
+        assert self.storage.get_all_cfgd_lun()
 
     def test_del_all(self):
-    	self.storage.lun_create()
-    	self.storage.lun_map()
-    	assert self.storage.del_all(['pytest_99']) == None
-
+        self.storage.lun_create()
+        self.storage.lun_map()
+        assert self.storage.del_all(['pytest_99']) == None
