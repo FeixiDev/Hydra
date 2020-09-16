a9755e09365399d9a349c28a056108b054e7a149
zane <zane.chen@feixitek.com>
2020-09-16 15:52:22 +0800

    Zane:add replay cmd test

--- test/test_main.py
+++ test/test_main.py
@@ -80,3 +80,31 @@ def test_del_s_id():
     p.stdin.write(b'y')
     stdout_data, stderr_data = p.communicate()
     assert stdout_data != None
+
+
+def test_rpl_a():
+    out = output('rpl -a')
+    assert out != None
+
+
+def test_rpl_t():
+    log = r'''[2020/09/16 15:04:00] [1600239840] [T] [DATA] [input] [user_input] [] [{'valid': '1', 'cmd': 'lun -id 1 -s unittest'}]|
+[2020/09/16 15:04:00] [1600239840] [T] [INFO] [info] [start] [8100624309] [Start to connect 10.203.1.231 via Telnet]|
+[2020/09/16 15:04:00] [1600239840] [F] [DATA] [value] [dict] [data for telnet connect] [{'host': '10.203.1.231', 'port': 23, 'username': 'root', 'password': 'Feixi@123'}]|
+[2020/09/16 15:04:01] [1600239840] [T] [INFO] [info] [start] [2063149801] [Start to connect 10.203.1.199 via SSH]|
+[2020/09/16 15:04:01] [1600239840] [F] [DATA] [value] [dict] [data for SSH connect] [{'host': '10.203.1.199', 'port': 22, 'username': 'root', 'password': 'password'}]|
+[2020/09/16 15:04:01] [1600239840] [T] [INFO] [info] [start] [9231610804] [Start to connect 10.203.1.200 via SSH]|
+[2020/09/16 15:04:01] [1600239840] [F] [DATA] [value] [dict] [data for SSH connect] [{'host': '10.203.1.200', 'port': 22, 'username': 'root', 'password': 'password'}]|
+[2020/09/16 15:04:03] [1600239840] [T] [INFO] [info] [start] [] [Start to modify IQN on Host]|
+[2020/09/16 15:04:03] [1600239840] [T] [INFO] [info] [start] [] [Check up the status of session]|
+[2020/09/16 15:04:03] [1600239840] [F] [DATA] [STR] [V9jGOP1i] [] [9162083304]|
+[2020/09/16 15:04:03] [1600239840] [T] [OPRT] [cmd] [ssh] [9162083304] [iscsiadm -m session]|'''
+    with open('Hydra_log.log', 'a') as file:  
+        file.write(log)  
+    out = output('rpl -t 1600239840')
+    assert out != None
+
+
+def test_rpl_d():
+    out = output('rpl -d \'2020/09/16 15:04:00\' \'2020/09/16 16:00:00\'')
+    assert out != None
